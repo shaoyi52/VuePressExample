47bb07fe9e8a51bbc9774fc83d8f28e961ec896c
yuzhoufen <yuzhoufen@foresee.com.cn>
2023-09-25 16:01:36 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,46 @@
+import { defineUserConfig,defaultTheme } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Foo',
+        link: '/foo/',
+      },{
+        text: 'Bar',
+        link: '/bar/',
+      },
+      
+    ],
+    // 所有页面会使用相同的侧边栏
+    sidebar: [
+      // SidebarItem
+      {
+        text: 'Foo',
+        link: '/foo/',
+        children: [
+          // SidebarItem
+          {
+            text: 'github',
+            link: 'https://github.com',
+            children: [],
+          },
+          // 字符串 - 页面文件路径
+          '/foo/bar.md',
+        ],
+      },// 字符串 - 页面文件路径
+      '/bar/README.md',]
+  }),
+})
---
+++ docs/foo/readme.md
@@ -0,0 +1,9 @@
+---
+sidebar: 
+  - text: 快速上手
+    link: /zh/guide/getting-started.
+    children:
+      - text: 'github'
+        link: 'https://github.com'
+---
+README
