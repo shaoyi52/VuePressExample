0203056fa82f4dfc0548a80fe70fa13d06f5c86e
shaoyi52 <shaoyi52@users.noreply.github.com>
2024-05-22 08:45:56 +0000

    deploy: c470c507a4dfe154a75f120239ac9a529967d7c4

---
+++ assets/VSCode setting文件配置.html-8b4e2c3f.js
@@ -0,0 +1,12 @@
+import{_ as e,o as t,c as n,f as i,e as a}from"./app-86c50f4f.js";const s={},d=a(`<h1 id="vscode-setting-json配置" tabindex="-1"><a class="header-anchor" href="#vscode-setting-json配置" aria-hidden="true">#</a> VSCode setting.json配置</h1><h3 id="eslintrc配置" tabindex="-1"><a class="header-anchor" href="#eslintrc配置" aria-hidden="true">#</a> eslintrc配置</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code> &quot;editor.codeActionsOnSave&quot;:  {
+    // For ESLint
+    &quot;source.fixAll.eslint&quot;: true,
+  },
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="prettier配置" tabindex="-1"><a class="header-anchor" href="#prettier配置" aria-hidden="true">#</a> Prettier配置</h3>`,4);function r(o,c){return t(),n("div",null,[i(`
+ * @Author: yuzhoufen
+ * @Date: 2021-11-05 09:58:39
+ * @LastEditors: yuzhoufen
+ * @LastEditTime: 2021-11-05 10:03:02
+ * @Description: Do not edit
+ * @FilePath: \\my-apph:\\gitHub\\days_blog\\vscode配置\\VSCode setting文件配置.md
+`),d])}const u=e(s,[["render",r],["__file","VSCode setting文件配置.html.vue"]]);export{u as default};
