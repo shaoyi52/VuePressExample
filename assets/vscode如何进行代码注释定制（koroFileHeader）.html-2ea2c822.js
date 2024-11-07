26ff0c69d51925460fcb2c75d043fd3e8073679a
shaoyi52 <shaoyi52@users.noreply.github.com>
2024-11-07 04:02:59 +0000

    deploy: d38fc56f1c3b189fed2249799a347fe5b7080323

--- assets/vscode如何进行代码注释定制（koroFileHeader）.html-2ea2c822.js
+++
@@ -1,8 +0,0 @@
-import{_ as e,o as a,c as r,f as o,e as d}from"./app-bc70983f.js";const t={},i=d('<h1 id="vscode如何进行代码注释定制-korofileheader" tabindex="-1"><a class="header-anchor" href="#vscode如何进行代码注释定制-korofileheader" aria-hidden="true">#</a> vscode如何进行代码注释定制（koroFileHeader）</h1><h2 id="安装" tabindex="-1"><a class="header-anchor" href="#安装" aria-hidden="true">#</a> 安装</h2><h2 id="在插件中查询插件korofileheader-安装" tabindex="-1"><a class="header-anchor" href="#在插件中查询插件korofileheader-安装" aria-hidden="true">#</a> 在插件中查询插件koroFileHeader，安装</h2>',3);function s(n,c){return a(),r("div",null,[o(`
- * @Author: your name
- * @Date: 2021-10-28 14:53:33
- * @LastEditTime: 2021-10-28 14:56:53
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \\incometaxportalh:\\gitHub\\days_blog\\vscode配置\\vscode如何进行代码注释定制（koroFileHeader）.md
-`),i])}const _=e(t,[["render",s],["__file","vscode如何进行代码注释定制（koroFileHeader）.html.vue"]]);export{_ as default};
